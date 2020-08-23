c613b09f547d440a4061e7322d760b4fbebd6220
Tim Kopp <tim.kopp@cyber-balance.com>
2020-08-23 09:47:47 -0400

    added scaf

---
+++ Models/CodeTemplate.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOMData.Models
+{
+    [JsonObject(NamingStrategyType = typeof(DefaultNamingStrategy))]
+    public class CodeTemplate
+    {
+        public int TemplateID { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; } 
+        public string Type { get; set; }
+    }
+}
---
+++ Providers/DocProvider.cs
@@ -0,0 +1,25 @@
+﻿ 
+using SOMData.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+namespace SOMData.Providers
+{
+    public static class DocProvider
+    {
+        public static IEnumerable<CodeTemplate> GetTemplates(string Path)  
+        {
+            DirectoryInfo DI = new DirectoryInfo($"{Path}");
+            foreach (FileInfo file in DI.GetFiles("*.*"))
+            {
+                using (TextReader tr = File.OpenText(file.FullName))
+                { 
+                    yield return (new CodeTemplate() { Name = file.Name, Content = tr.ReadToEnd()});
+                }
+
+            }
+        }
+
+    }
+}
