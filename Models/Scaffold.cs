f1f6f3f9272207892b2ecfdf86440e238f7f8a28
Tim Kopp <tim.kopp@cyber-balance.com>
2020-08-29 15:03:17 -0400

    added snippets

---
+++ Models/Scaffold.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOMData.Models
+{
+    public class Scaffold
+    {
+        public int ScaffoldId { get; set; }
+        public string ModelName { get; set; }
+        public string Namespace { get; set; }
+        public string SaveDestination { get; set; }
+    } 
+}
